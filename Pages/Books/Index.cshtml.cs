74bf3cb4fcadcf290d7f31916a47349d25c7da61
MARCELA RALUCA PREDA <preda.marcela165334@student.ubbcluj.ro>
2020-11-29 15:18:22 +0200

    Add project files.

---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Preda_Marcela_Lab8.Data;
+using Preda_Marcela_Lab8.Models;
+
+namespace Preda_Marcela_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Preda_Marcela_Lab8.Data.Preda_Marcela_Lab8Context _context;
+
+        public IndexModel(Preda_Marcela_Lab8.Data.Preda_Marcela_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Preda_Marcela_Lab8.Data;
+using Preda_Marcela_Lab8.Models;
+
+namespace Preda_Marcela_Lab8.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Preda_Marcela_Lab8.Data.Preda_Marcela_Lab8Context _context;
+
+        public IndexModel(Preda_Marcela_Lab8.Data.Preda_Marcela_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Publisher = await _context.Publisher.ToListAsync();
+        }
+    }
+}
